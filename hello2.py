aa3925d85f8f77ee77fdc6347499f8fcf9b3f21d
jeferG <jefer960428@gmail.com>
2025-04-16 15:18:58 -0500

    resuelto

--- hello2.py
+++ hello2.py
@@ -3,8 +3,5 @@ print("hello world")
 print("hello world")
 print("cremeria")
 print("hello")
-<<<<<<< HEAD
 print("esto es de la rama main")
-=======
 print("esto tambien es un cambio")
->>>>>>> develop
